29006933c3a9644ed7d82992c77aad0e632ef730
jaylimbachiya43 <109168679+jaylimbachiya43@users.noreply.github.com>
2024-01-16 07:19:09 +0530

    link expierd

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import FinalComponent from "@/components/finalcomponent";
 export default function Home() {
   return (
     <main>
-      <FinalComponent />
+      {/* <FinalComponent /> */}
+    <h1 className="flex justify-center content-center items-center ">Link Expired</h1>
     </main>
   );
 }
